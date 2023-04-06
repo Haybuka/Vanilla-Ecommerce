4a1741e48a99097db03cd284488dbb432b902540
haybuka <paschal.nduleo@gmail.com>
2023-04-06 18:03:20 +0100

    Update : added email notification, and event handler

---
+++ index.js
@@ -0,0 +1,28 @@
+let form = document.querySelector("form");
+
+//submit event handler
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let emailInput = this[0];
+  if (emailInput.value == "" || !emailInput.value.includes("@")) {
+    console.log("empty input field");
+    createNotification("Invalid email address", "notif-error");
+    setTimeout(() => {
+      document.querySelector(".notif").remove();
+    }, 3000);
+  } else {
+    createNotification("Subscribed", "notif-success");
+    setTimeout(() => {
+      document.querySelector(".notif").remove();
+    }, 3000);
+  }
+  emailInput.value = ''
+});
+
+let createNotification = (text, notifType) => {
+  let body = document.querySelector("body");
+  const notif = document.createElement("p");
+  notif.setAttribute("class", `notif ${notifType}`);
+  notif.innerHTML = text;
+  body.append(notif);
+};
